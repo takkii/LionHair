54ff9e1547700f023c75be679846df5b75b1d2c8
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-12 19:31:14 +0900

    Default.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "all.h"
+
+int main() {
+    const string str = "Hello, World !";
+    cout << str << endl;
+
+    // C++ version info.
+    long cpp17 = 201703L;
+    long cpp14 = 201402L;
+    long cpp11 = 201103L;
+    long cpp98 = 199711L;
+
+    // long type → string type.
+    string cpp_ver = to_string(__cplusplus);
+
+    if (cpp_ver == to_string(cpp17))
+        cout << "現在の C++ : C++17" << endl;
+    else if (cpp_ver == to_string(cpp14))
+        cout << "現在の C++ : C++14" << endl;
+    else if (cpp_ver == to_string(cpp11))
+        cout << "現在の C++ : C++11" << endl;
+    else if (cpp_ver == to_string(cpp98))
+        cout << "現在の C++ : C++98" << endl;
+    else
+        cout << "現在の C++ : pre-standard C++" << endl;
+    return 0;
+}
