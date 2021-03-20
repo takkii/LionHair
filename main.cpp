dc82049b9e93d2d732c10d080ccad165ca4ea817
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-03-20 13:34:01 +0900

    Update, class add.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include "all.h"
 
-int main() {
+class LionHair {
+    public:
     const string str = "Hello, World !";
-    printf("%s\n", str.c_str());
+};
+    int main() {
 
-    // C++ version info.
-    auto cpp17 = 201703L;
-    auto cpp14 = 201402L;
-    auto cpp11 = 201103L;
-    auto cpp98 = 199711L;
+        LionHair l;
+        printf("%s\n", l.str.c_str());
+        // C++ version info.
+        auto cpp17 = 201703L;
+        auto cpp14 = 201402L;
+        auto cpp11 = 201103L;
+        auto cpp98 = 199711L;
 
-    // long type → string type.
-    const string cpp_ver = to_string(__cplusplus);
+        // long type → string type.
+        const string cpp_ver = to_string(__cplusplus);
 
-    if (cpp_ver == to_string(cpp17))
-        cout << "現在の C++ : C++17" << endl;
-    else if (cpp_ver == to_string(cpp14))
-        cout << "現在の C++ : C++14" << endl;
-    else if (cpp_ver == to_string(cpp11))
-        cout << "現在の C++ : C++11" << endl;
-    else if (cpp_ver == to_string(cpp98))
-        cout << "現在の C++ : C++98" << endl;
-    else
-        cout << "現在の C++ : pre-standard C++" << endl;
-    return 0;
-}
+        if (cpp_ver == to_string(cpp17))
+            cout << "現在の C++ : C++17" << endl;
+        else if (cpp_ver == to_string(cpp14))
+            cout << "現在の C++ : C++14" << endl;
+        else if (cpp_ver == to_string(cpp11))
+            cout << "現在の C++ : C++11" << endl;
+        else if (cpp_ver == to_string(cpp98))
+            cout << "現在の C++ : C++98" << endl;
+        else
+            cout << "現在の C++ : pre-standard C++" << endl;
+        return 0;
+    }
